9152413b2621fdda620c85d7291a8dc489efdbea
Emrullah Lüleci <emrullahluleci@gmail.com>
2016-02-19 23:35:41 +0200

    fix gitignore

---
+++ log/log.go
@@ -0,0 +1,39 @@
+package log
+
+import (
+	"github.com/Sirupsen/logrus"
+)
+
+var logger = logrus.New()
+
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Debug(args ...interface{}) {
+	logger.Debug(args...)
+}
+
+func Warning(args ...interface{}) {
+	logger.Warning(args...)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Panic(args ...interface{}) {
+	logger.Panic(args...)
+}
+
+func Fatal(args ...interface{}) {
+	logger.Fatal(args...)
+}
+
+func SetLevel(level logrus.Level) {
+	logger.Level= level
+}
+
+func AddHook(hook logrus.Hook) {
+	logger.Hooks.Add(hook)
+}
