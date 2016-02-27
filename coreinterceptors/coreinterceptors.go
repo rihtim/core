c95ebe608684c469bb2dcedaf34a60de70d3f71c
Emrullah Lüleci <emrullahluleci@gmail.com>
2016-02-28 00:07:50 +0200

    add path validation

--- coreinterceptors/coreinterceptors.go
+++ coreinterceptors/coreinterceptors.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 	"github.com/rihtim/core/utils"
 	"github.com/rihtim/core/messages"
+	"strings"
 )
+
+var AllowedPaths []string
 
 var Expander = func(user map[string]interface{}, message messages.Message) (response messages.Message, err *utils.Error) {
 
@@ -20,5 +23,24 @@ var MethodNotAllowed = func(user map[string]interface{}, message messages.Messag
 
 var NotFound = func(user map[string]interface{}, message messages.Message) (response messages.Message, err *utils.Error) {
 	err = &utils.Error{http.StatusNotFound, http.StatusText(http.StatusNotFound)}
+	return
+}
+
+var PathValidator = func(user map[string]interface{}, message messages.Message) (response messages.Message, err *utils.Error) {
+
+	if AllowedPaths == nil {
+		response = message
+		return
+	}
+
+	for _, v := range AllowedPaths {
+		if strings.Index(message.Res, v) != -1 {
+			response = message
+			return
+		}
+	}
+
+	err = &utils.Error{http.StatusBadRequest, "Path is not valid."}
+
 	return
 }
