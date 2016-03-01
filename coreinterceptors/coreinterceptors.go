aab213651252d80d16ca7cbf0cdc6627c9100bc1
Emrullah Lüleci <emrullahluleci@gmail.com>
2016-03-01 11:05:22 +0200

    convert expander to intercepor

--- coreinterceptors/coreinterceptors.go
+++ coreinterceptors/coreinterceptors.go
@@ -7,27 +7,20 @@ import (
 	"github.com/rihtim/core/messages"
 	"github.com/rihtim/core/modifier"
 	"github.com/rihtim/core/constants"
-	"fmt"
 )
 
 var AllowedPaths []string
 
 var Expander = func(user map[string]interface{}, message messages.Message) (response messages.Message, err *utils.Error) {
 
-	//	log.Info("Expander interceptor called.")
-	fmt.Println("expander")
-	fmt.Println(message)
 	response = message
-
 	if message.Parameters["expand"] != nil {
 		expandConfig := message.Parameters["expand"][0]
-		fmt.Println(response.Body)
 		if _, hasDataArray := response.Body[constants.ListIdentifier]; hasDataArray {
 			response.Body, err = modifier.ExpandArray(response.Body, expandConfig)
 		} else {
 			response.Body, err = modifier.ExpandItem(response.Body, expandConfig)
 		}
-		fmt.Println(response.Body)
 	}
 
 	return
