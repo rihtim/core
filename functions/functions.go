d91d7f9a24cf8637b5bcf27055f807cda214f90a
Emrullah Luleci <emrullah.luleci@ingbank.com.tr>
2017-11-14 11:20:20 +0300

    change function param names

--- functions/functions.go
+++ functions/functions.go
@@ -7,7 +7,7 @@ import (
 	"github.com/rihtim/core/dataprovider"
 )
 
-type FunctionHandler func(req messages.Message, rs requestscope.RequestScope, db dataprovider.Provider) (res messages.Message, editedRs requestscope.RequestScope, err *utils.Error)
+type FunctionHandler func(req messages.Message, rs requestscope.RequestScope, dp dataprovider.Provider) (resp messages.Message, editedRs requestscope.RequestScope, err *utils.Error)
 
 type FunctionController interface {
 	Contains(path string) bool
