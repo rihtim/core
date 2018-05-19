1a950a7b753eaec248e7bd71c5a221629fad462a
Emrullah Lüleci <emrullahluleci@gmail.com>
2018-05-19 15:44:09 +0300

    add get int parameter to messages

--- messages/message.go
+++ messages/message.go
@@ -2,7 +2,9 @@ package messages
 
 import (
 	"io"
+	"strconv"
 	"mime/multipart"
+	"github.com/rihtim/core/utils"
 )
 
 type Message struct {
@@ -28,6 +30,26 @@ func (m Message) GetParameter(key string) (value string, contains bool) {
 	values, contains := m.Parameters[key]
 	if contains {
 		value = values[0]
+	}
+	return
+}
+
+func (m Message) GetIntParameter(key string) (valueAsNumber int, contains bool, err *utils.Error) {
+	if m.Parameters == nil {
+		contains = false
+		return
+	}
+
+	values, contains := m.Parameters[key]
+	if contains {
+		number, convertErr := strconv.Atoi(values[0])
+		if convertErr != nil {
+			err = &utils.Error{
+			    Message: convertErr.Error(),
+			}
+			return
+		}
+		valueAsNumber = number
 	}
 	return
 }
